5bd49d044a009756303c856f7b7fea5255addb3b
Marta P <mapa.startuj@gmail.com>
2017-12-18 21:25:38 +0100

    initial commit

---
+++ components/App.js
@@ -0,0 +1,44 @@
+var contacts = [{
+        id: 1,
+        firstName: 'Jan',
+        lastName: 'Nowak',
+        email: 'jan.nowak@example.com',
+    },
+    {
+        id: 2,
+        firstName: 'Adam',
+        lastName: 'Kowalski',
+        email: 'adam.kowalski@example.com',
+    },
+    {
+        id: 3,
+        firstName: 'Zbigniew',
+        lastName: 'Koziol',
+        email: 'zbigniew.koziol@example.com',
+    },
+    {
+        id: 4,
+        firstName: 'Tomasz',
+        lastName: 'Kinel',
+        email: 'tomasz.kinel@example.com',
+    }
+];
+
+//pusty obiekt dla formularza dodającego kontakt
+var contactForm = {
+    firstName: '',
+    lastName: '',
+    email: ''
+};
+
+//klasa App która będzie renderowana do diva o klasie app.
+var App = React.createClass({
+    render: function() {
+        return (
+            React.createElement('div', { className: 'app' },
+                React.createElement(ContactForm, { contact: contactForm }, {}),
+                React.createElement(Contacts, { items: contacts }, {})
+            )
+        );
+    }
+});
